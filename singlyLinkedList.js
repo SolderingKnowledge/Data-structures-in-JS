c35fdad953f3a441bc0866b9a763332b48efba64
Aikol Tashbolot uulu <aikoltashbolotuulu@Aikols-iMac.local>
2019-07-08 20:13:20 -0700

    singly

---
+++ singlyLinkedList.js
@@ -0,0 +1,92 @@
+class Node {
+    constructor(val){
+      this.val = val;
+      this.next = null;
+    }
+  }
+  
+  class SLL{
+    constructor(){
+      this.head= null;
+      this.tail= null;
+      this.length = 0;
+    }
+    push(val){
+      let newNode = new Node(val);
+      if(!this.head){//if it's empty list
+        this.head = newNode;
+        this.tail = this.head;//both pointing to new Node but below we're updating the tail
+      }else{//if list is not empty
+        this.tail.next = newNode;
+        this.tail = newNode;//updating the tail 
+      }
+      this.length++;
+      return this;//return the whole List
+    }
+    pop(){
+      // a ->     b ->          c
+      //                     current
+      //        newtail
+      let current = this.head;
+      let newTail = current;//they both start at the begining
+      while(current.next){
+        newTail = current;
+        current = current.next;//move current forward
+      }
+      this.tail = newTail;//prev is going to be current
+      this.tail.next = null;//deleting meaning assigning to null
+      this.length--;
+      return current;
+    }
+  }
+  
+  
+  
+  
+  
+  let sll = new SLL();
+  sll.push('a');
+  sll.push('b');
+  sll.push('c');
+  sll.push('d');
+  sll;
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  // let start = new Node('Can');
+  // start.next = new Node('you');
+  // start.next.next = new Node("help");
+  // start.next.next.next = new Node("me");
+  // start.next.next.next.next = new Node("?");
